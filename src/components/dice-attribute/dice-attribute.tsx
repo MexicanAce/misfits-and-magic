b6296102edfe8bcbe1929fd6d7b22b311542cf2e
Nicolas Villanueva <nicolasvillanueva@msn.com>
2023-03-22 20:57:54 +0000

    Updates to misfits and magic

--- src/components/dice-attribute/dice-attribute.tsx
+++ src/components/dice-attribute/dice-attribute.tsx
@@ -1,9 +1,7 @@
 import { Dispatch, SetStateAction, useState } from 'react';
 import { Character, CharacterStat } from '../../types/Character';
 import { DiceType } from '../../types/Dice';
-import Modal from '@mui/material/Modal';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
+import DiceSelectModal from '../dice-select-modal/dice-select-modal';
 import './dice-attribute.scss';
 
 function DiceAttribute({
@@ -22,17 +20,6 @@ function DiceAttribute({
   const [openDiceSelectModal, setOpenDiceSelectModal] = useState(false);
   const handleOpen = () => setOpenDiceSelectModal(true);
   const handleClose = () => setOpenDiceSelectModal(false);
-  const style = {
-    position: 'absolute' as 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-  };
 
   let diceShape = <></>;
   const diceGradient = (
@@ -130,21 +117,13 @@ function DiceAttribute({
         </div>
       </div>
 
-      <Modal
+      <DiceSelectModal
+        character={character}
+        setCharacter={setCharacter}
         open={openDiceSelectModal}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
-      </Modal>
+        handleClose={handleClose}
+        attribute={attribute}
+        characterStat={characterStat} />
     </div>
   );
 }
--- src/components/dice-select-modal/dice-select-modal.tsx
+++ src/components/dice-select-modal/dice-select-modal.tsx
@@ -1,19 +1,52 @@
-import { Dispatch, SetStateAction } from 'react';
-import { Character } from '../../types/Character';
+import { Dispatch, SetStateAction, useState } from 'react';
+import { Character, CharacterStat } from '../../types/Character';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
 import './dice-select-modal.scss';
 
 function DiceSelectModal({
   character,
   setCharacter,
+  open,
+  handleClose,
+  attribute,
+  characterStat,
 }: {
   character: Character;
   setCharacter: Dispatch<SetStateAction<Character>>;
+  open: boolean;
+  handleClose: () => void;
+  attribute: string;
+  characterStat: CharacterStat;
 }) {
+  const modalStyle = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+    color: 'black',
+    borderRadius: '10%',
+  };
+
   return (
-    <div className="model-container">
-      <img className="character" src={character.imageUrl} alt="character-png" />
-      <img className="stand" src="src/assets/stand.png" alt="stand-png" />
-    </div>
+    <Modal
+      open={open}
+      onClose={handleClose}
+    >
+      <Box sx={modalStyle}>
+        <div className="modal-content">
+          <div className="cancel" onClick={handleClose}>X</div>
+          <button className="modal-action-button">Cast Magic (+D4)</button>
+          <button className="modal-action-button">Normal Action</button>
+          <button className="modal-button">Edit Die</button>
+        </div>
+      </Box>
+    </Modal>
   );
 }
 
